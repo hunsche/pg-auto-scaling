c307bbb7bbeee78f2bcb45b3e9c9790e741b1d04
Lucas Viecelli <lviecelli199@gmail.com>
2017-10-11 01:47:03 -0300

    dd

--- docker/master/docker-entrypoint-initdb.d/edit-conf.sh
+++ docker/master/docker-entrypoint-initdb.d/edit-conf.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 sed -i 's/#wal_level = replica/wal_level = logical/g' /var/lib/postgresql/data/postgresql.conf
+createdb beers_db
+psql beers_db -c "
+    CREATE TABLE beers (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR (100),
+        price NUMERIC (12, 2)
+    );
+
+    CREATE PUBLICATION pub_beers FOR TABLE beers;
+"
--- docker/slave/docker-entrypoint-initdb.d/edit-conf.sh
+++ docker/slave/docker-entrypoint-initdb.d/edit-conf.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 sed -i 's/#wal_level = replica/wal_level = logical/g' /var/lib/postgresql/data/postgresql.conf
+createdb beers_db
+psql beers_db -c "
+    CREATE TABLE beers (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR (100),
+        price NUMERIC (12, 2)
+    );
+
+    CREATE SUBSCRIPTION sub_beers
+    CONNECTION 'host=172.17.0.1 port=5532 dbname=beers_db' 
+    PUBLICATION pub_beers;"
